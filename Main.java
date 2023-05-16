5b70cacd42d076cd27319ca599665cd01db0eee2
Michael Coulter <coulte12@go.stockton.edu>
2023-05-16 15:46:19 -0400

    1

---
+++ Main.java
@@ -0,0 +1,9 @@
+package Rock_Paper_Scissors_Game;
+
+
+// Make a 2-D simulation game of Rock, Paper, and Scissors. Make 10 of each characters and have them fight each other.
+// Rocks turn Scissors into Rocks.
+// Scissors turn Papers into Scissors.
+// Papers turn Rocks into Papers.
+// If two of the same characters fight each other, nothing changes.
+// If two different characters fight each other and tie, nothing changes.
